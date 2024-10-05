a471343c9f6dcba7576df51204977b930c91344f
DarkGames26 <fredericpadilla26@gmail.com>
2024-10-05 01:16:10 -0400

    Initial commit

---
+++ includes/resourceloader/PortableInfoboxResourceLoaderModule.php
@@ -0,0 +1,15 @@
+<?php
+
+use MediaWiki\ResourceLoader\Context;
+use MediaWiki\ResourceLoader\FileModule;
+
+class PortableInfoboxResourceLoaderModule extends FileModule {
+
+	/** @inheritDoc */
+	protected function getLessVars( Context $context ) {
+		$lessVars = parent::getLessVars( $context );
+		$lessVars[ 'responsibly-open-collapsed'] =
+			(bool)$this->getConfig()->get( 'PortableInfoboxResponsiblyOpenCollapsed' );
+		return $lessVars;
+	}
+}
